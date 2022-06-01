d838309ebb5d6221f8bbb592f23fee16b9313423
Raman Maksimchuk <dotnet044@gmail.com>
2022-06-01 17:44:49 +0300

    More unit tests

--- BackendApi.Tests/Unit/MultiValueParamModelBinderTests.cs
+++ BackendApi.Tests/Unit/MultiValueParamModelBinderTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Primitives;
 using Poq.BackendApi.Binders;
+using Poq.BackendApi.Models;
+using System.Collections;
 
 namespace Poq.BackendApi.Tests.Unit;
 
@@ -66,6 +69,7 @@ public class MultiValueParamModelBinderTests
         var valueProviderMock = new Mock<IValueProvider>();
         contextMock.SetupGet(x => x.ValueProvider).Returns(valueProviderMock.Object);
 
+        // Arrange : Value provider returns nothing
         valueProviderMock.Setup(x => x.GetValue(parameterName)).Returns(ValueProviderResult.None);
 
         var state = new ModelStateDictionary();
@@ -78,5 +82,122 @@ public class MultiValueParamModelBinderTests
         Assert.True(state.Count == 0); // model was not set
         Assert.False(state.ContainsKey(parameterName));
         contextMock.VerifyGet(x => x.ModelState, Times.Never());
+    }
+
+    [Fact]
+    public async Task BindModelAsync_CollectionFormatIsMulti_Success()
+    {
+        // Arrange
+        var service = new MultiValueParamModelBinder();
+        string parameterName = "highlight";
+
+        var contextMock = new Mock<ModelBindingContext>();
+        contextMock.SetupGet(x => x.ModelName).Returns(parameterName);
+        var valueProviderMock = new Mock<IValueProvider>();
+        contextMock.SetupGet(x => x.ValueProvider).Returns(valueProviderMock.Object);
+
+        var state = new ModelStateDictionary();
+        contextMock.SetupGet(x => x.ModelState).Returns(state);
+
+        ModelBindingResult result = ModelBindingResult.Failed();
+        contextMock.SetupSet(x => x.Result = It.IsAny<ModelBindingResult>())
+            .Callback<ModelBindingResult>(x =>
+            {
+                result = x;
+                contextMock.SetupGet(x => x.Result).Returns(result);
+            });
+
+        // Arrange : Multiple parameters with the same name
+        ValueProviderResult values = new ValueProviderResult(new StringValues(new string[] { "1", "22", "333" }));
+        valueProviderMock.Setup(x => x.GetValue(parameterName)).Returns(values);
+
+        // Act
+        await service.BindModelAsync(contextMock.Object);
+
+        // Assert
+        Assert.True(state.Count > 0); // there is a model...
+        Assert.True(state.ContainsKey(parameterName)); // ...with exact parameter name...
+        contextMock.VerifyGet(x => x.ModelState, Times.Once()); //...and the state was setup once.
+
+        contextMock.VerifySet(x => x.Result = It.IsAny<ModelBindingResult>());
+        Assert.True(result.IsModelSet);
+
+        var actualModel = result.Model;
+        Assert.NotNull(result.Model);
+        Assert.IsAssignableFrom<MultiValueParam>(actualModel);
+
+        MultiValueParam actual = actualModel as MultiValueParam;
+        Assert.NotNull(actual);
+        Assert.True(actual.Count > 1);
+        Assert.Equal(values.Length, actual.Count);
+        Assert.All(values, x => actual.Contains(x));
+    }
+
+    public static IEnumerable<object[]> SingleParameterValues =>
+        new object[][]
+        {
+            new object[] { "1", 1 },
+            new object[] { "word", 1 },
+            new object[] { "1#2", 1 },
+            new object[] { "1,2,3", 3 },
+            new object[] { "1 2 3", 3 },
+            new object[] { @"1\2\3", 3 },
+            new object[] { "1|2|3", 3 },
+            new object[] { "1$2$3", 1 },
+            new object[] { "a,bb,wordC,wordD", 4 },
+            new object[] { ",1", 1 },
+            new object[] { "1,", 1 },
+            new object[] { ",1,2,", 2 },
+        };
+
+    [Theory]
+    [MemberData(nameof(SingleParameterValues))]
+    public async Task BindModelAsync_SingleParam_Success(string bindingData, int expextedCount)
+    {
+        // Arrange
+        var service = new MultiValueParamModelBinder();
+        string parameterName = "highlight";
+
+        var contextMock = new Mock<ModelBindingContext>();
+        contextMock.SetupGet(x => x.ModelName).Returns(parameterName);
+        var valueProviderMock = new Mock<IValueProvider>();
+        contextMock.SetupGet(x => x.ValueProvider).Returns(valueProviderMock.Object);
+
+        var state = new ModelStateDictionary();
+        contextMock.SetupGet(x => x.ModelState).Returns(state);
+
+        ModelBindingResult result = ModelBindingResult.Failed();
+        contextMock.SetupSet(x => x.Result = It.IsAny<ModelBindingResult>())
+            .Callback<ModelBindingResult>(x =>
+            {
+                result = x;
+                contextMock.SetupGet(x => x.Result).Returns(result);
+            });
+
+        // Arrange : Single parameter with with separated values
+        ValueProviderResult values = new ValueProviderResult(new StringValues(bindingData));
+        valueProviderMock.Setup(x => x.GetValue(parameterName)).Returns(values);
+
+        // Act
+        await service.BindModelAsync(contextMock.Object);
+
+        // Assert : The model is set to the state and the state was setup once.
+        Assert.True(state.Count > 0);
+        Assert.True(state.ContainsKey(parameterName));
+        contextMock.VerifyGet(x => x.ModelState, Times.Once());
+
+        // Assert : The result has a model
+        contextMock.VerifySet(x => x.Result = It.IsAny<ModelBindingResult>());
+        Assert.True(result.IsModelSet);
+
+        // Assert : Valid MVC model type
+        var actualModel = result.Model;
+        Assert.NotNull(result.Model);
+        Assert.IsAssignableFrom<MultiValueParam>(actualModel);
+
+        // Assert : Valid length of produced collection
+        MultiValueParam actual = actualModel as MultiValueParam;
+        Assert.NotNull(actual);
+        Assert.Equal(expextedCount, actual.Count);
     }
 }
